710e4fb2f898ce285d335d697ced9781433ca6dd
EDICON <hslee.edicon@gmail.com>
2019-11-14 15:11:05 +0900

    -App name: MBM App

--- socket-app/src/environments/version.ts
+++ socket-app/src/environments/version.ts
@@ -1,14 +1,14 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-    "dirty": false,
-    "raw": "bdc5647",
-    "hash": "bdc5647",
+    "dirty": true,
+    "raw": "e6e09de-dirty",
+    "hash": "e6e09de",
     "distance": null,
     "tag": null,
     "semver": null,
-    "suffix": "bdc5647",
+    "suffix": "e6e09de-dirty",
     "semverString": null,
-    "version": "0.1.4"
+    "version": "0.1.5"
 };
 /* tslint:enable */
